83fa8c14279c5d213ad9afd334d60501def82514
Thomas <thomasps2002@gmail.com>
2025-05-02 12:50:18 +0530

    Add project files.

---
+++ WebApp.Client/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebApp.Client.Dto;
+
+namespace WebApp.Client.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:5177/api/Customer/GetAllCustomer");
+            var response = await client.SendAsync(request);
+            List<CustomerResponseDto> customers = new();
+            if(response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                customers = JsonConvert.DeserializeObject<List<CustomerResponseDto>>(result);
+
+            }
+            return View(customers);
+        }
+    }
+}
---
+++ WebApp.Client/Dto/CustomerResponseDto.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.Client.Dto
+{
+    public class CustomerResponseDto
+    {
+
+        public int CustomerId { get; set; }
+        public string customName { get; set; }
+        public string ProductName { get; set; }
+        public int OrderId { get; set; }
+        public int Qty { get; set; }
+        public int UnitPrice { get; set; }
+        public int TotalPrice { get; set; }
+
+    }
+}
